6508cf6bdc66850f2ebf66f7a90d604ff12e410e
Alisa Maiboroda <74829706+alisamaiboroda@users.noreply.github.com>
2021-03-11 10:58:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let buttonOperation = document.getElementsByClassName("button-operation");
+let input1 = document.getElementById("number-1");
+let input2 = document.getElementById("number-2");
+
+function makeOperation(operationCode) {
+  console.log(operationCode);
+  console.log(operationCode === "+");
+
+  let number1 = Number(input1.value);
+  let number2 = Number(input2.value);
+  let result;
+  if (operationCode === "+") {
+    result = number1 + number2;
+  } else if (operationCode === "-") {
+    result = number1 - number2;
+  } else if (operationCode === "*") {
+    result = number1 * number2;
+  } else if (operationCode === "/") {
+    result = number1 / number2;
+  } else if (operationCode === "C") {
+    result = "";
+    input1.value = "";
+    input2.value = "";
+  }
+  document.getElementById("result-input").value = result;
+}
+
+function onOperationButtonClick(eventObject) {
+  let clickedElement = eventObject.currentTarget;
+  let operation = clickedElement.dataset.operation;
+  makeOperation(operation);
+}
+
+for (let i = 0; i < buttonOperation.length; i++) {
+  let button = buttonOperation[i];
+  button.addEventListener("click", onOperationButtonClick);
+}
